0fc9398aa3919e66fa18dc651e9cb4854d38cddd
suatgungor <suat.gungor@mymail.champlain.edu>
2025-02-26 15:48:27 -0500

    Initial commit of week5 lab

---
+++ HTML Elements and Web Requests.ps1
@@ -0,0 +1,16 @@
+﻿#$Scraped_page = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.36/ToBeScraped.html
+
+#$scraped_page.Links.Count
+
+#$scraped_page.Links
+
+#$Scraped_page.Links | Select-Object outerText, href | Format-List
+
+#$h2s = $scraped_page.ParsedHtml.body.getElementsByTagName("h2") | Select-Object outerText
+
+#$h2s
+
+#$divs1 = $scraped_page.ParsedHtml.body.getElementsByTagName("div") | Where {
+    $_.getAttributeNode("class").value -ilike "div-1" } | select innerText
+
+#$divs1
