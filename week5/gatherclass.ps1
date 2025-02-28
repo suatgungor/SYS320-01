a6d90baa72aeb451bd90a59812f40d1aef9c7709
suatgungor <suat.gungor@mymail.champlain.edu>
2025-02-28 18:56:31 -0500

    Add .ps1 lab files for week5

---
+++ week5/gatherclass.ps1
@@ -0,0 +1,48 @@
+﻿function gatherClasses(){
+    $page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.36/Courses.html
+
+    $trs = $page.ParsedHtml.getElementsByTagName('tr')
+
+    $FullTable = @()
+
+    for ($i = 1; $i -lt $trs.length; $i++) { 
+
+        $tds = $trs[$i].getElementsByTagName('td')
+        $Times = $tds[5].innerText.Split('-')
+
+        $FullTable += [PSCustomObject]@{
+            "Class Code"   = $tds[0].innerText;
+            "Title"        = $tds[1].innerText;
+            "Days"         = $tds[4].innerText;
+            "Time Start"   = $Times[0].Trim();
+            "Time End"     = $Times[1].Trim();
+            "Instructor"   = $tds[6].innerText;
+            "Location"     = $tds[9].innerText;
+        }
+    }
+    
+    return $FullTable
+}
+
+function daysTranslator($FullTable){
+
+    for ($i = 0; $i -lt $FullTable.length; $i++){
+        $Days = @()
+
+        if ($FullTable[$i].Days -like "*M*") { $Days += "Monday" }
+
+        if ($FullTable[$i].Days -like "*T[,W,F]*") { $Days += "Tuesday"} 
+    
+        elseif ($FullTable[$i].Days -like "*T*") {$Days += "Tuesday" }
+      
+        if ($FullTable[$i].Days -like "*W*") { $Days += "Wednesday" }
+
+        if ($FullTable[$i].Days -like "*Th*") { $Days += "Thursday" }
+
+        if ($FullTable[$i].Days -like "*F*") { $Days += "Friday" }
+
+        $FullTable[$i].Days = $Days
+    }
+
+    return $FullTable
+}
