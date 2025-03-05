1753ee2c022b8b9af7e766aee8dff3ca1668afcb
suatgungor <suat.gungor@mymail.champlain.edu>
2025-03-05 04:24:05 -0500

    Added Turn_to_Menu folder with ApacheLogs1.ps1 and main.ps1

---
+++ Turn_to_Menu/ApacheLogs1.ps1
@@ -0,0 +1,67 @@
+﻿function ApacheLogs1() { 
+    $logsNotFormatted = Get-Content C:\xampp\apache\logs\access.log 
+    $stableRecords = @()
+
+    for ($i=0; $i -lt $logsNotFormatted.Count; $i++) {
+        $words = $logsNotFormatted[$i] -split " "
+
+        $stableRecords += [PSCustomObject]@{
+            "IP"        = $words[0]
+            "Time"      = $words[3].Trim("[")
+            "Method"    = $words[5].Trim('"')
+            "Page"      = $words[6]
+            "Protocol"  = $words[7].Trim('"')
+            "Response"  = $words[8]
+            "Referrer"  = $words[10].Trim('"')
+            "Client"    = $words[11..($words.Count - 1)] -join " "
+        }
+    }
+
+    return $stableRecords | Where-Object { $_.IP -like "10.*" } | Select-Object -Last 10
+}
+function getMatchingLines($contents, $lookline){
+
+$allines = @()
+$splitted =  $contents.split([Environment]::NewLine)
+
+for($j=0; $j -lt $splitted.Count; $j++){  
+ 
+   if($splitted[$j].Length -gt 0){  
+        if($splitted[$j] -ilike $lookline){ $allines += $splitted[$j] }
+   }
+
+}
+
+return $allines
+}
+
+function getFailedLogins($timeBack){
+  
+  $failedlogins = Get-EventLog security -After (Get-Date).AddDays("-"+"$timeBack") | Where { $_.InstanceID -eq "4625" }
+
+  $failedloginsTable = @()
+  for($i=0; $i -lt $failedlogins.Count; $i++){
+
+    $account=""
+    $domain="" 
+
+    $usrlines = getMatchingLines $failedlogins[$i].Message "*Account Name*"
+    $usr = $usrlines[1].Split(":")[1].trim()
+
+    $dmnlines = getMatchingLines $failedlogins[$i].Message "*Account Domain*"
+    $dmn = $dmnlines[1].Split(":")[1].trim()
+
+    $user = $dmn+"\"+$usr;
+
+    $failedloginsTable += [pscustomobject]@{"Time" = $failedlogins[$i].TimeGenerated; `
+                                       "Id" = $failedlogins[$i].InstanceId; `
+                                    "Event" = "Failed"; `
+                                     "User" = $user;
+                                     }
+
+    }
+
+    return $failedloginsTable
+}
+
+$stableRecords = ApacheLogs1 $stableRecords | Format-Table -AutoSize -Wrap
---
+++ Turn_to_Menu/main.ps1
@@ -0,0 +1,88 @@
+﻿. (Join-Path $PSScriptRoot ApacheLogs1.ps1)
+
+clear
+
+$Prompt = "`n"
+$Prompt += "Please choose your operation:`n"
+$Prompt += "1 - Display last 10 Apache logs`n"
+$Prompt += "2 - Display last 10 failed logins`n"
+$Prompt += "3 - List At-Risk Users`n"
+$Prompt += "4 - Visit champlain.edu`n"
+$Prompt += "5 - Exit`n"
+
+
+$looping = $true
+
+while($looping){
+
+    
+    Write-Host $Prompt | Out-String
+    $choice = Read-Host 
+
+
+    if($choice -eq 1){
+        Write-Host "Showing last 10 Apache logs..."
+            ApacheLogs1 | Select-Object -Last 10 | Format-Table -AutoSize
+        
+    }
+
+    elseif($choice -eq 2){
+
+         $numFailures = Read-Host "Enter the number of failed logins you want to see"
+
+         Write-Host "These are the last $numFailures failed attempts"
+         $failedLogins = getFailedLogins 90  
+         $failedLogins | Select-Object -Last $numFailures | Format-Table
+    }
+
+    elseif($choice -eq 3){
+           
+        $days = Read-Host "How many days do you wana check for a failed login attemp"
+        $failedLogins = getFailedLogins $days
+
+        $userFailures = @{}
+
+        foreach ($entry in $failedLogins) {
+            $user = $entry.User
+            if ($userFailures.ContainsKey($user)) {
+                $userFailures[$user] += 1
+            } else {
+                $userFailures[$user] = 1
+            }
+        }
+
+        $atRiskUsers = @()
+        $dayscheck = Read-Host "How many failed login attemps do you wana check for"
+        foreach ($user in $userFailures.Keys) {
+            if ($userFailures[$user] -gt $dayscheck) {
+                $atRiskUsers += "$user - Failed Logins: $($userFailures[$user])"
+            }
+        }
+
+        if ($atRiskUsers.Count -gt 0) {
+            Write-Host "At-Risk Users (More than $dayscheck failed logins in the last $days days):"
+            $atRiskUsers | ForEach-Object { Write-Host $_ }
+            
+            Write-Host "These are the failed attempts..."
+
+            $failedLogins | Select-Object -Last $dayscheck | Format-Table
+        } else {
+            Write-Host "No at-risk users found in the last $days days."
+        }
+    }
+    elseif($choice -eq 4){
+        $chromeProcess = Get-Process 'chrome' -ErrorAction SilentlyContinue
+
+        if ($chromeProcess) {
+            $chromeProcess | Stop-Process
+        }
+        else {
+            Write-Host "Opening champlain.edu"
+            Start-Process "chrome.exe" "https://www.champlain.edu"
+        }
+        }
+    elseif($choice -eq 5){
+        Write-Host "BYE!"
+        $looping = $false 
+    }
+    }
