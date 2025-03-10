4efd02a86d83db0156d0d8d3492333e1341fe8d7
suatgungor <suat.gungor@mymail.champlain.edu>
2025-03-09 22:54:36 -0400

    Add midterm scripts and access log

---
+++ Midterm.ps1
@@ -0,0 +1,61 @@
+﻿function Get-IOCs {
+    $Scraped_page = Invoke-WebRequest -TimeoutSec 10 -Uri "http://10.0.17.6/IOC.html"
+    $rows = $Scraped_page.ParsedHtml.getElementsByTagName("tr")
+    $results = @()
+
+        for ($i = 1; $i -lt $rows.length; $i++) {
+            $cells = $rows[$i].getElementsByTagName("td")
+
+            $pattern = $cells[0].innerText.Trim()
+            $desc = $cells[1].innerText.Trim()
+
+            $results += "$pattern`t$desc"
+        
+
+    }
+
+    return $results
+}
+
+function Get-ApacheLogs {
+    $logPath = "C:\Users\champuser\Desktop\Midterm\access.log"
+    $results = @()
+
+    foreach ($line in Get-Content $logPath) {
+        $parts = $line.Split(" ")
+
+        $ip = $parts[0]
+        $time = $line -split "\[|\]" | Select-Object -Index 1
+        $method = $parts[5].Trim('"')
+        $page = $parts[6]
+        $protocol = $parts[7].Trim('"')
+        $response = $parts[8]
+        $referrer = $line -split '"' | Select-Object -Index 3
+
+        $results += "$ip`t$time`t$method`t$page`t$protocol`t$response`t$referrer"
+    }
+
+    return $results
+}
+
+function Match-LogsWithIOCs {
+    $logs = Get-ApacheLogs
+    $iocs = Get-IOCs
+    $matched = @()
+
+    foreach ($log in $logs) {
+        $fields = $log -split "`t"
+        $page = $fields[3]
+
+        foreach ($ioc in $iocs) {
+            $iocPattern = ($ioc -split "`t")[0]
+            if ($page -like "*$iocPattern*") {
+                $matched += $log
+                break
+            }
+        }
+
+    }
+
+    return $matched
+}
---
+++ main.ps1
@@ -0,0 +1,61 @@
+﻿. (Join-Path $PSScriptRoot Midterm.ps1)
+
+
+$Prompt = "`n"
+$Prompt += "Please choose your operation:`n"
+$Prompt += "1 - Challange 1`n"
+$Prompt += "2 - Challange 2`n"
+$Prompt += "3 - Challange 3`n"
+$Prompt += "4 - Exit`n"
+
+$looping = $true
+
+
+while($looping){
+
+    
+    Write-Host $Prompt | Out-String
+    $choice = Read-Host 
+
+
+    if($choice -eq 1){
+
+        $challange1 = Get-IOCs
+
+        Write-Host "This is the output for challange 1: Write a PowerShell function that obtains IOC from the given web page."
+
+        foreach ($line in $challange1) {
+            Write-Output $line
+        }
+    }
+
+    elseif($choice -eq 2){
+
+        $challenge2 = Get-ApacheLogs
+
+        Write-Host "This is the output for challange 2: Write a PowerShell function that obtains apache access logs."
+
+        foreach ($line in $challenge2) {
+            Write-Output $line
+        }
+
+    }
+
+    elseif($choice -eq 3){
+
+    $logs = Get-ApacheLogs
+
+    $iocs = Get-IOCs
+
+    $suspiciousLogs = Match-LogsWithIOCs -logs $logs -iocs $iocs
+
+    foreach ($line in $suspiciousLogs) {
+        Write-Output $line
+    }
+    }
+
+    elseif($choice -eq 4){
+    Write-Host "BYE!"
+    $looping = $false 
+    }
+    }
